7744f1e50d4ee71cae45f4c42eeaec59e0b1c698
nico-amsterdam <16443840+nico-amsterdam@users.noreply.github.com>
2023-11-26 23:32:13 +0100

    Update mini.sh
    Replace uglifyjs with terser

--- mini.sh
+++ mini.sh
@@ -1,3 +1,4 @@
-cd gh-pages/js
-( echo "// AwesompleteUtil - Nico Hoogervorst - MIT license"; uglifyjs --compress --mangle --mangle-props --mangle-regex="/^_[^l][^i]|^bound|sourceId|targetId|dataField|listQuery|queryParam|awe|xhr|btnId/" --source-map awesomplete-util.min.js.map --source-map-url awesomplete-util.min.js.map -- awesomplete-util.js ) | cat > awesomplete-util.min.js
-cd ../..
+cd js
+../node_modules/terser/bin/terser --compress --mangle --mangle-props "regex='/^_[^l][^i]|^bound|sourceId|targetId|dataField|listQuery|queryParam|awe|xhr|btnId/'" --output awesomplete-util.min.js --source-map "url='awesomplete-util.min.js.map'" -- awesomplete-util.js
+sed -i '1 i\// AwesompleteUtil - Nico Hoogervorst - MIT license' awesomplete-util.min.js
+cd ..
