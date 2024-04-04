73f7da556cfa1e958c61e2aef7a1f37736e6d9b5
Nico <n.g.c.hoogervorst@gmail.com@users.noreply.github.com>
2024-04-05 00:18:54 +0200

    Added example in docs: dismissable chips

--- js/scroll.js
+++ js/scroll.js
@@ -1,1 +1,2 @@
+// On small devices, the virtual keyboard covers the list with suggestions. So, if the suggestions are shown, move up the input field to make room.
 window.addEventListener('awesomplete-open', (el) => { if (window.innerWidth < 577 && window.innerHeight < 800) el.target.scrollIntoView(); });
